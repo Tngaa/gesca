da00581990c94cb62651f441aea55955e9403494
Tunga <tungalag.natsagdorj@gmail.com>
2019-08-02 17:10:35 +0200

    rien changer

--- src/main/java/com/tunga/gesca/controllers/UserController.java
+++ src/main/java/com/tunga/gesca/controllers/UserController.java
@@ -1,12 +1,17 @@
 package com.tunga.gesca.controllers;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tunga.gesca.entities.AnnualSale;
 import com.tunga.gesca.entities.User;
 import com.tunga.gesca.repositories.UserRepository;
 
@@ -21,5 +26,22 @@ public class UserController {
 	public List<User> getUsers() {
 		
 		return (List<User>)userRepository.findAll();
+	}
+	
+	
+	@GetMapping("/{userId}")
+	public User getUser(@PathVariable Integer userId) {
+		User user = null;
+		
+		Optional<User> optUser = userRepository.findById(userId);
+		
+		if(optUser.isPresent()) {
+			user = optUser.get();
+			
+			List<AnnualSale> sortedAnnualSales = user.getAnnualSales().stream().sorted(Comparator.comparingInt(AnnualSale::getYear).reversed()).collect(Collectors.toList());
+			user.setAnnualSales(sortedAnnualSales);
+		}
+		
+		return user;
 	}
 }
--- src/main/java/com/tunga/gesca/repositories/UserRepository.java
+++ src/main/java/com/tunga/gesca/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.tunga.gesca.repositories;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.tunga.gesca.entities.User;
 
 public interface UserRepository extends CrudRepository< User, Integer> {
-
+	
+	@Query ("FROM User WHERE id = :userId")
+	public Optional<User> findById(@Param("userId") Integer userId);
 }
